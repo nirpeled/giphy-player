78e5dc56402680745095e8772c3786f6305208df
Nir Peled <nir@wisdo.com>
2016-07-13 03:10:09 +0300

    [FIX] improve the player using pure css techniques

--- app/components/player/player.js
+++ app/components/player/player.js
@@ -1,44 +1,11 @@
 import _ from 'lodash';
 import React from 'react';
 import helpers from '../../helpers/helpers.js';
-import classNames from 'classnames';
 
 // constants
 import iconsConstants from '../../constants/icons-constants.js';
 
 var Player = React.createClass({
-
-    getInitialState: function() {
-
-        return {
-            orientation: this.calculateOrientation()
-        }
-
-    },
-
-    componentDidMount: function() {
-
-        window.addEventListener('orientationchange', this.handleOrientationChange, true);
-
-    },
-
-    componentWillUnmount: function() {
-
-        window.removeEventListener('orientationchange', this.handleOrientationChange, true);
-
-    },
-
-    calculateOrientation: function() {
-
-        return (window.innerWidth > window.innerHeight) ? 'horizontal' : 'vertical';
-
-    },
-
-    handleOrientationChange: function() {
-
-        this.setState({orientation: this.calculateOrientation()})
-
-    },
 
     handleClose: function(e) {
        
@@ -57,29 +24,16 @@ var Player = React.createClass({
         helpers.logger('[Player] render');
 
         var props = this.props,
-            state = this.state,
             image = _.get(props, 'images.original.url'),
-            videoWidth = parseInt(_.get(props, 'images.original.width')),
-            videoHeight = parseInt(_.get(props, 'images.original.height')),
-            videoRatio = videoWidth / videoHeight,
-            videoOrientation = (videoWidth > videoHeight) ? 'horizontal' : 'vertical',
             style = {
-                //width: _.min([videoWidth, window.innerWidth]),
-                height: '100%'
+                width: _.min([window.innerWidth, window.innerHeight]),
+                backgroundImage: 'url(' + image + ')'
             };
 
-        console.log('state', state);
-        console.log('videoWidth', videoWidth);
-        console.log('videoHeight', videoHeight);
-        console.log('videoRatio', videoRatio);
-        console.log('videoOrientation', videoOrientation);
-        console.log('window.innerWidth', window.innerWidth);
-        console.log('style', style);
-
         return (
-            <section className={classNames('box-player', state.orientation)}>
+            <section className="box-player">
                 <span className="close" onClick={this.handleClose}><i className={iconsConstants.CLOSE} /></span>
-                <img className={videoOrientation} src={image} style={style}/>
+                <span className="video" style={style} />
             </section>
         );
     }
